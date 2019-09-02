3dcd1b23d1cb6eb19fb65543da7dc660750255d2
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-09-02 19:02:54 +0530

    Final, tested

--- auto_backup.py
+++ auto_backup.py
@@ -5,20 +5,31 @@ paths = []
 with open("data_backup_paths.txt", "r") as file:
   for line in file:
     paths.append(line.rstrip())
-print (paths)
 
 # copy files to central backup location, compress
 system("mkdir sys_auto_backup")
-system("cd sys_auto_backup")
 for path in paths:
-  system("cp -r "+path+" ./")
+  system("cp -r "+path+" ./sys_auto_backup")
 system("zip sys_auto_backup.zip sys_auto_backup")
 
 # check drive upload status
+system("gdrive list > gdrive_status.txt")
+file = open ("gdrive_status.txt","r")
 
-# new approach : 
-#   store gdrive list output in a file
-#   read line by line from file
-#   if "file_name" in line
-#     use id token (first token by space delim)
-text = system("gdrive list")
+file_id = None
+for line in file:
+  if "sys_auto_backup.zip" in line:
+    file_id = line.partition(' ')[0]
+    break
+
+# upload/update file to drive
+if file_id == None :
+  system("gdrive upload sys_auto_backup.zip")
+else:
+  system("gdrive update "+file_id+" sys_auto_backup.zip")
+
+# cleanup
+system("rm -rf sys_auto_backup.zip")
+system("rm -rf sys_auto_backup")
+system("rm -rf gdrive gdrive_status.txt")
+
